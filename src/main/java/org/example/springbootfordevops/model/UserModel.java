9ba754ce3459f37e0a23878df877fece97bbe74c
SandeepEddirs <sandeep.k@eddirs.com>
2026-09-01 11:52:17 +0530

    docker is added with the evn file setup

--- src/main/java/org/example/springbootfordevops/model/UserModel.java
+++ src/main/java/org/example/springbootfordevops/model/UserModel.java
@@ -1,6 +1,5 @@
 package org.example.springbootfordevops.model;
 
-import org.apache.catalina.User;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
